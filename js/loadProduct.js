2c5c7dddb20973a40646b491ea3b7a536eb61b38
Gorge <lvlup9119@gmail.com>
2022-12-31 20:07:33 +0200

    My_forth commit. Make some ecsperemets for box_product(make with masive).

---
+++ js/loadProduct.js
@@ -0,0 +1,28 @@
+import { myShowProd, myOneProd, myCheckProduct } from "./functionProd.js";
+
+let products = [
+    {
+        "name": "Ginho",
+        "waight": "240",
+        "time": "90"
+    },
+    {
+        "name": "Trio",
+        "waight": "290",
+        "time": "120"
+    },
+    {
+        "name": "Rouse",
+        "waight": "120",
+        "time": "60"
+    }
+]
+
+let x = "Hello";
+
+// myCheckProduct(products);
+myOneProd(products);
+// console.log(myShowProd(products));
+
+
+
